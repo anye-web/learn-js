74e1a313bd3ef75fe92583cd1e6ff21adeb58f1e
anye-world <frujuniorp@gmail.com>
2021-06-16 01:26:08 -0200

    awseome

---
+++ Awesome Tips JS and Css/Readmore or Less Button/index.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector(".btn"),
+    dropdown = document.querySelector(".dropdown");
+
+btn.addEventListener("click", function() {
+    dropdown.style.display = "block";
+    console.log("hellp")
+})
